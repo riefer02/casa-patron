a836f0757e24f45d533769c4784949d3a8650318
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-01-08 11:59:52 -0600

    initialize mdx pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,11 +30,19 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-mdx`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/content/posts`,
       },
     },
     `gatsby-transformer-sharp`,
